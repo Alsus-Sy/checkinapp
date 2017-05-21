c7f82b0bf2a815bd08386468ac13a1a4e4d9da5d
Alsus Sy <alsus85@gmail.com>
2017-05-21 15:44:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## A Simple CheckIn Page
+
+This is just a very simple app to track customers check in for various services available in the store. 
---
+++ app.js
@@ -0,0 +1,73 @@
+var express        = require("express"),
+    app            = express(),
+    bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose"),
+    methodOverride = require("method-override");
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/checkin_app");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+//== HOW TO SETUP A TEST DB
+// MONGOOSE/MODEL CONFIG
+var displaySchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  email: String,
+  service: String,
+  extraservice: String,
+  tech: String  
+});
+
+var Display = mongoose.model("Display", displaySchema);
+
+// === RESTFUL ROUTES ===
+app.get("/", function(req, res) {
+  res.redirect("/displays");
+});
+
+//TEST DATA
+Display.remove({}, function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
+//=========
+
+// === INDEX ===
+app.get("/displays", function(req, res) {
+  Display.find({}, function(err, displays) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {displays: displays});
+    }
+  });
+});
+
+// === NEW ===
+app.get("/displays/signin", function(req, res) {
+  res.render("signin");
+});
+
+// === CREATE ===
+app.post("/displays", function(req, res) {
+  // create new info
+  Display.create(req.body.display, function(err, newDisplay) {
+    if (err) {
+      res.render("signin");
+    } else {
+      // then redirect to the index
+      res.redirect("/displays");
+    }
+  });
+});
+
+
+// === SERVER STARTING CONFIG
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Your app's server is running!!");
+});
